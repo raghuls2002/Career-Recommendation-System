e71d9c6da49252e8954c5e135542269adbbfa101
raghuls2002 <raghul.sureshkumar@gmail.com>
2023-04-02 23:04:40 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -5,46 +5,42 @@ Created on Sun Apr  2 20:49:11 2023
 @author: Raghul S
 """
 
-import preprocessing
-data = preprocessing.data
-target = preprocessing.target
+from preprocessing import X_train_s, y_train, X_test_s, y_test
 
-from sklearn.model_selection import train_test_split
+X_train = X_train_s
+X_test = X_test_s
+
 from sklearn.metrics import confusion_matrix,accuracy_score
-
-X=data.drop([target], axis=1)
-y=data[target]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
 
 from sklearn.ensemble import RandomForestRegressor
 
 forest=RandomForestRegressor(random_state=10)
 forest.fit(X_train, y_train)
-y_predict = forest.predict(X_test)
-
+rf_y_pred = forest.predict(X_test)
 
 from sklearn.tree import DecisionTreeClassifier
 
 dtree = DecisionTreeClassifier(random_state=1)
 dtree = dtree.fit(X_train, y_train)
 
-y_pred = dtree.predict(X_test)
-cm = confusion_matrix(y_test,y_pred)
-accuracy = accuracy_score(y_test,y_pred)
-print("confusion matrics=",cm)
+dt_y_pred = dtree.predict(X_test)
+dt_cm = confusion_matrix(y_test,dt_y_pred)
+dt_accuracy = accuracy_score(y_test,dt_y_pred)
+print("\n\nDecision Tree Classifier : ")
+print("\nconfusion matrics=",dt_cm)
 print("  ")
-print("accuracy=",accuracy*10)
+print("accuracy=",dt_accuracy*10)
 
 from xgboost import XGBClassifier
 
-xgb = XGBClassifier(random_state = 42, learning_rate=0.02, n_estimators=300)
+xgb = XGBClassifier(random_state = 40, learning_rate=0.03, n_estimators=250)
 xgb.fit(X_train, y_train)
+
 xgb_y_pred = xgb.predict(X_test)
 xgb_cm = confusion_matrix(y_test,xgb_y_pred)
 xgb_accuracy = accuracy_score(y_test,xgb_y_pred)
-print("confusion matrics=",xgb_cm)
+print("\n\nXGB Classifier : ")
+print("\nconfusion matrics=",xgb_cm)
 print("  ")
 print("accuracy=",xgb_accuracy*10)
 
@@ -52,10 +48,12 @@ from sklearn import svm
 
 svm = svm.SVC()
 svm.fit(X_train, y_train)
+
 svm_y_pred = svm.predict(X_test)
 svm_cm = confusion_matrix(y_test,svm_y_pred)
 svm_accuracy = accuracy_score(y_test,svm_y_pred)
-print("confusion matrics=",svm_cm)
+print("\n\nSVM Classifier : ")
+print("\nconfusion matrics=",svm_cm)
 print("  ")
 print("accuracy=",svm_accuracy*10)
 
